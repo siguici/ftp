a91e5eda7e12341ed4bbc64095b0f828e54de1fd
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-03-05 16:25:17 +0100

    :hammer: Improve the FTP Application

--- src/FtpApplication.php
+++ src/FtpApplication.php
@@ -1,6 +1,10 @@
 <?php namespace Ftp;
 
 class FtpApplication {
+	use FtpEvents;
+
+	public function __construct(protected string $name) {}
+
     public function setCwd(string $cwd): self {
         if (!is_dir($cwd)) {
             throw new \InvalidArgumentException("$cwd is not a directory");
@@ -14,6 +18,20 @@ class FtpApplication {
     }
 
     public function run(int $argc, array $argv): void {
+    	if ($argc !== count($argv)) {
+    		$this->do('raise', new FtpException('Wrong arguments count', FtpException::INVALID_ARGUMENT));
+    		return;
+    	}
+
+    	if (1 > $argc) return;
+
+    	$name = realpath(array_shift($argv));
+    	if ($name !== $this->name) {
+    		$this->do('raise', new FtpException("Unabled to run $this->name as $name"));
+    		return;
+    	}
+    	$argc = count($argv);
+
         if ($argc > 1) {
             match ($argv[1]) {
                 'put' => $this->put(...array_slice($argv, 2)),
@@ -33,5 +51,4 @@ Commands:
 
 USAGE;
     }
-
 }
