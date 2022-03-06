533758a9d43054cd5d070788c6d5de4065176193
SIGUI Kessé Emmanuel <developer@sikessem.com>
2022-03-06 14:58:18 +0100

    :art: Add FTP environment variables handler

--- ftp.php
+++ ftp.php
@@ -2,7 +2,8 @@
 
 use Ftp\{
 	FtpClient,
-	FtpServer
+	FtpServer,
+	FtpDotenv
 };
 
 if (!function_exists('ftp_client')) {
@@ -14,5 +15,25 @@ if (!function_exists('ftp_client')) {
 if (!function_exists('ftp_server')) {
 	function ftp_server(string $host, int $port = 21, int $timeout = 90, bool $secure = false): FtpServer {
 		return new FtpServer($host, $port, $timeout, $secure);
+	}
+}
+
+if (!\function_exists('env')) {
+	function env(?string $key = null, mixed $default = null): mixed {
+		static $env;
+		if (!isset($env)) {
+			$env = new FtpDotenv();
+		}
+		return isset($key) ? $env->get($key, $default) : $env;
+	}
+}
+
+if(!\function_exists('ftp_env')) {
+	function ftp_env(?string $key = null, mixed $default = null): mixed {
+		static $env;
+		if (!isset($env)) {
+			$env = new FtpDotenv();
+		}
+		return isset($key) ? $env->get($key, $default) : $env;
 	}
 }
